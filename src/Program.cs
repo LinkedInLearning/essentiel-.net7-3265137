bf27e06578c3b1f937d35dc76e6529bbee253723
Labasse <labasse@gmail.com>
2023-03-27 17:14:08 +0000

    Traiter les répertoires (début)

---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace persist_01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Chemins, standard, extensions
+
+      // Manipulation répertoires/fichiers
+
+      // Exploration                       
+    }
+  }
+}
