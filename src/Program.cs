a00b3aa116e405c15dad33c817a4859ab5f193c9
Labasse <labasse@gmail.com>
2023-03-27 20:26:11 +0000

    Explorer le réseau (début)

---
+++ src/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+// Linux - lancement terminal : sudo dotnet run
+namespace com_03
+{
+  class Program
+  {
+    public static void AfficherAdresses(string titre, IEnumerable<IPAddress> adresses)
+    {
+      Console.WriteLine(titre);
+      foreach (var adr in adresses)
+      {
+        Console.WriteLine($"\t- {adr}");
+      }
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
