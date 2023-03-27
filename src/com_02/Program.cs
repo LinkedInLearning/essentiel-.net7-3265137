4ec45d228b23536f5df6dd433f3587c010770326
Labasse <labasse@gmail.com>
2023-03-27 20:21:41 +0000

    Transmettre sur le web (fin)

--- src/com_02/Program.cs
+++ src/com_02/Program.cs
@@ -1,11 +1,35 @@
-﻿namespace com_02;
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 // Lancer le serveur web dans le terminal
 // Dans src/com_02web : dotnet run
-class Program
+
+namespace com_02
 {
-    static void Main(string[] args)
+  class Program
+  {
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+      using MultipartFormDataContent formulaire = new();
+      using FileStream flux = new("profil.jpg", FileMode.Open);
+      using StreamContent photo = new(flux);
+
+      photo.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+
+      formulaire.Add(new StringContent("Le chat"), "titre");
+      formulaire.Add(photo, "photo", Path.GetFileName(flux.Name));
+
+      using HttpClient http = new() { BaseAddress = new Uri("http://localhost:5000") };
+      var réponse = await http.PostAsync($"/", formulaire);
+
+      réponse.EnsureSuccessStatusCode();
+
+      var contenu = await réponse.Content.ReadAsStringAsync();
+
+      Console.WriteLine(contenu);
     }
+  }
 }
