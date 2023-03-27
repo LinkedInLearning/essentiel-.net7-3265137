060ed5b43c398c0cb86339b7a6969baa2acc3251
Labasse <labasse@gmail.com>
2023-03-27 20:57:49 +0000

    Connecter un client (début)

---
+++ src/com_05/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace com_05;
+
+// Lancement du serveur dans le terminal
+// Dans com_04srv : dotnet run
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello, World!");
+  }
+}
