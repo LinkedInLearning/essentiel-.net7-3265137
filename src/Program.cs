d9669086a0b73e0cb0e2f9447be926440c6ada5c
Labasse <labasse@gmail.com>
2023-03-27 17:15:06 +0000

    Traiter les répertoires (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace persist_01
 {
@@ -7,10 +8,22 @@ namespace persist_01
     static void Main(string[] args)
     {
       // Chemins, standard, extensions
+      var dossier = Path.Combine("./bin", "sous-dossier");
+      var fichier = "obj/toto.txt";
+      var fichier2 = Path.ChangeExtension(fichier, ".md");
+
+      Console.WriteLine(dossier);
+      Console.WriteLine(fichier2);
 
       // Manipulation répertoires/fichiers
+      Directory.CreateDirectory(dossier);
+      var dir = new DirectoryInfo("./bin");
 
       // Exploration                       
+      foreach (var sousDossier in dir.GetDirectories())
+      {
+        Console.WriteLine($"{sousDossier.FullName} {sousDossier.LastAccessTimeUtc}");
+      }
     }
   }
 }
