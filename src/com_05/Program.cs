66d61e3c74d830bbad19d95361e6ce6da1268302
Labasse <labasse@gmail.com>
2023-03-27 21:03:34 +0000

    Connecter un client (fin)

--- src/com_05/Program.cs
+++ src/com_05/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace com_05;
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace com_05;
 
 // Lancement du serveur dans le terminal
 // Dans com_04srv : dotnet run
@@ -6,6 +10,38 @@ class Program
 {
   static void Main(string[] args)
   {
-    Console.WriteLine("Hello, World!");
+    try
+    {
+      using TcpClient tcp = new();
+      var cnx = tcp.ConnectAsync("localhost", 10_000);
+
+      Console.Write("Votre message : ");
+      var msg = Console.ReadLine();
+
+      cnx.Wait();
+      try
+      {
+        var flux = tcp.GetStream();
+        using StreamWriter w = new(flux);
+
+        w.WriteLine(msg);
+        w.Flush();
+
+        using StreamReader r = new(flux);
+
+        while (!r.EndOfStream)
+        {
+          Console.WriteLine(r.ReadLine());
+        }
+      }
+      catch (Exception e)
+      {
+        Console.Error.WriteLine($"Communication : {e.Message}");
+      }
+    }
+    catch (Exception e)
+    {
+      Console.Error.WriteLine($"Connexion : {e.Message}");
+    }
   }
 }
