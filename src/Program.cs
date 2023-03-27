e16045fb5522d5ad6077a41839a2b79a2888692e
Labasse <labasse@gmail.com>
2023-03-27 14:39:51 +0000

    Intégrer les dates et le temps (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,7 @@ namespace info_02
   {
     static void Main(string[] args)
     {
-      DateTime début = new(year: 2021, month: 08, day: 10, hour: 12, 0, 0);
+      DateTimeOffset début = new(year: 2021, month: 08, day: 10, hour: 12, 0, 0, offset: TimeSpan.FromHours(1));
       TimeSpan durée = new(hours: 1, minutes: 30, 0);
       var fin = début + durée;
       var attente = début - DateTime.Now;
