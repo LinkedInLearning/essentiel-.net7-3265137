36601c9c2aae8f83ee9a91669194ed83621edc34
Labasse <labasse@gmail.com>
2023-03-27 17:20:47 +0000

    Lire et écrire des fichiers (début)

---
+++ src/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace persist_02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var prénom = "Sylvain";
+      var nom = "LABASSE";
+      var valeur1 = 5;
+      var valeur2 = 3.2;
+
+      try
+      {
+
+      }
+      catch (Exception e)
+      {
+        Console.Error.WriteLine(e.Message);
+      }
+    }
+  }
+}
