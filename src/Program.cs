c51a5bcde557096ff7befa8258b1a947a2c8080b
Labasse <labasse@gmail.com>
2023-03-27 17:25:35 +0000

    Compresser (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System.IO.Compression;
+using System.Linq;
 
 namespace persist_03
 {
@@ -15,8 +16,30 @@ namespace persist_03
       }
 
       // Création d'une archive
+      ZipFile.CreateFromDirectory("./bin", archive.FullName);
+
+      ZipFile.ExtractToDirectory(archive.FullName, "./obj", overwriteFiles: true);
 
       // Ajout d'un fichier
+      using (FileStream zip = new(archive.FullName, FileMode.Open))
+      using (ZipArchive flux = new(zip, ZipArchiveMode.Update))
+      {
+        var liste = flux.Entries.ToArray();
+        var index = flux.CreateEntry("index.txt");
+
+        using (StreamWriter writer = new(index.Open()))
+        {
+          foreach (var fichier in liste)
+          {
+            writer.WriteLine(
+                "{0} {1:0.00}%",
+                fichier.FullName,
+                (double)fichier.CompressedLength / (double)fichier.Length
+            );
+          }
+        }
+      }
+
     }
   }
 }
