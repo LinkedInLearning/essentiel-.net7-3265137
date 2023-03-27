e590d8bb93816a0a7d55ad89114c8dc1d4a7e662
Labasse <labasse@gmail.com>
2023-03-27 17:28:33 +0000

    Sécuriser par chiffrement (début)

---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace persist_04
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Symétrique
+      using (FileStream f = new("obj/fichier.sec", FileMode.Create))
+      using (StreamWriter writer = new(f))
+      {
+        writer.WriteLine("Le roi est nu");
+      }
+
+      using (FileStream f = new("obj/fichier.sec", FileMode.Open))
+      using (StreamReader reader = new(f))
+      {
+        Console.WriteLine(reader.ReadToEnd());
+      }
+
+      // Asymétrique
+      var message = Encoding.ASCII.GetBytes("Le roi est nu");
+
+      Console.WriteLine(Encoding.ASCII.GetString(message));
+    }
+  }
+}
