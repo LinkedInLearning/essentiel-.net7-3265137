583721ba23d380c45ef5715098151f0884bc83c4
Labasse <labasse@gmail.com>
2023-03-28 14:09:54 +0000

    Manipuler le texte (début)

---
+++ src/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace info_01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var liste = @"pique 
+                cœur
+                trèfle
+                carreau";
+
+      foreach (var couleur in liste)
+      {
+        Console.WriteLine(couleur);
+      }
+
+      /*            
+      var text = "Nombres";
+      var sep = " : ";
+
+      for(var i=1; i<=7; i++) {
+          text += $"{sep}{i}";
+          sep=", ";
+      }
+      Console.WriteLine(text);
+      text = text.Insert(7, " de 1 à 7");
+      Console.WriteLine(text);
+      */
+    }
+  }
+}
