752112287609feddcad74fadcc9e3dd5fc221814
Labasse <labasse@gmail.com>
2023-03-28 05:34:44 +0000

    Explorer le contenu du code par réflexion (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -8,14 +8,23 @@ namespace ctrl_03
     {
       Produit p = new("Tongs", 2.30m);
 
+      Console.WriteLine(p.GetType().Assembly);
+      Console.WriteLine(p.GetType().Name);
+
       foreach (var prop in typeof(Produit).GetProperties())
       {
-        Console.WriteLine(prop.Name);
+        var infos = prop.GetCustomAttributes(typeof(InfosAttribute), false) as InfosAttribute[] ?? Array.Empty<InfosAttribute>();
+
+        Console.Write(infos.Length > 0 ? infos[0].Nom : prop.Name);
+        Console.Write(" : ");
+        Console.WriteLine(prop.GetGetMethod()?.Invoke(p, Array.Empty<object>()));
       }
     }
   }
+  [Infos("C'est un produit")]
   public record Produit(string Nom, decimal Prix)
   {
+    [Infos("Le prix TTC")]
     public decimal PrixTTC => Prix * 1.2m;
   }
 }
