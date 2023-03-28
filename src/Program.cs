d06df1ab84689d2848948daeaeccf66bf1a3d1c6
Labasse <labasse@gmail.com>
2023-03-28 17:50:18 +0000

    Traiter les objets immuables (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -4,25 +4,31 @@
   {
     static void Main(string[] args)
     {
-      DateTimeOffset début = new DateTimeOffset(year: 2021, month: 08, day: 10, hour: 12, 0, 0, offset: TimeSpan.FromHours(1));
+      DateTimeOffset début = new DateTimeOffset(year: 2023, month: 08, day: 10, hour: 12, 0, 0, offset: TimeSpan.FromHours(1));
       TimeSpan       durée = new TimeSpan(hours: 1, minutes: 30, 0);
-      DateTimeOffset fin = début + durée;
+      DateTimeOffset fin   = début + durée;
       TimeSpan       attente = début - DateTimeOffset.Now;
 
       Console.WriteLine($"RDV {début:f} - {fin:F} dans {attente:dd\\jhh\\hmm}");
       Console.WriteLine($"RDV {début:g} - {fin:G} dans {attente:g}"); // DateTime : s
       Console.WriteLine($"RDV le {début:d} à {début:t} jusqu'à {fin:T} dans {attente:G}");
 
-      List<object> occurrences = new() { };
+      var rdv = new Rdv(début, durée, "Réunion");
+      List<Rdv> occurrences = new() { rdv };
 
       for (var i = 1; i < 10; i++)
       {
-        occurrences.Add(début + TimeSpan.FromDays(i * 7));
+        occurrences.Add(rdv with { Début = début + TimeSpan.FromDays(i * 7) });
       }
       foreach (var occ in occurrences)
       {
         Console.WriteLine(occ);
       }
+    }
+    public record Rdv(DateTimeOffset Début, TimeSpan Durée, string Titre)
+    {
+      public DateTimeOffset Fin => Début + Durée;
+      public TimeSpan Attente => Début - DateTimeOffset.Now;
     }
   }
 }
