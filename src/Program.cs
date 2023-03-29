cdc7fbd5f89dcd3dfafe5c0febe5e9bba36a2035
Labasse <labasse@gmail.com>
2023-03-29 08:56:17 +0200

    Manipuler les images (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,24 @@
 ﻿namespace media_03;
 
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Versioning;
+
 class Program
 {
+  [SupportedOSPlatform("windows")]
   static void Main(string[] args)
   {
-    Console.WriteLine("Hello world!");
+    using var img = Image.FromFile("chat.jpeg");
+
+    img.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+    using var vignette = img.GetThumbnailImage(
+        img.Width / 10,
+        img.Height / 10,
+        () => false,
+        IntPtr.Zero
+    );
+    vignette.Save("vignette-chat.png", ImageFormat.Png);
   }
 }
