1de84f4caabb079c6db5af9847c9aef68021bac6
Labasse <labasse@gmail.com>
2023-03-30 15:35:52 +0200

    Disposer de ressources (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,31 @@
-﻿namespace media_01
+﻿using System;
+using System.Resources;
+using System.IO;
+using System.Reflection;
+using System.Globalization;
+using System.Threading;
+
+namespace media_01
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Salut !");
-      Console.WriteLine("Au revoir");
+      var img = Assembly.GetExecutingAssembly().GetManifestResourceStream("chat");
+
+      using (FileStream fichier = new("chat2.jpeg", FileMode.Create))
+      {
+        img?.CopyTo(fichier);
+      }
+      var culture = CultureInfo.CreateSpecificCulture("fr-FR");
+
+      Thread.CurrentThread.CurrentCulture = culture;
+      Thread.CurrentThread.CurrentUICulture = culture;
+
+      var res = new ResourceManager("strings", Assembly.GetExecutingAssembly());
+
+      Console.WriteLine(res.GetString("hello"));
+      Console.WriteLine(res.GetString("bye"));
     }
   }
 }
