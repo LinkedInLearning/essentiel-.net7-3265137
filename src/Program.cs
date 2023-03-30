eb19320681c187cee51c25f5c5a9cb066070c050
Labasse <labasse@gmail.com>
2023-03-30 15:55:48 +0200

    Dessiner du texte (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Runtime.Versioning;
 
@@ -11,10 +12,15 @@ namespace media_05
     {
       using var vignette = Image.FromFile("vignette-chat.png");
       using var gCadre = Graphics.FromImage(vignette);
+      using Pen crayon = new(Brushes.Red, 1) { DashStyle = DashStyle.Dash };
       Rectangle rcCadre = new(x: 295, y: 7, width: 270, height: 270);
+      using Font police = new(FontFamily.GenericSansSerif, 10, FontStyle.Regular);
+      using StringFormat centré = new() { Alignment = StringAlignment.Center };
 
-      gCadre.DrawRectangle(Pens.Red, rcCadre);
+      gCadre.DrawRectangle(crayon, rcCadre);
 
+      rcCadre.Y += rcCadre.Height;
+      gCadre.DrawString("Zone de découpage de l'image", police, Brushes.Red, rcCadre, centré);
       vignette.Save("cadre-chat.png", ImageFormat.Png);
     }
   }
