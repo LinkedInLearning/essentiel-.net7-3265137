c0ce7ad296997d8099579a12d5a193d99ee6506b
Labasse <labasse@gmail.com>
2023-03-30 16:25:51 +0200

    Envoyer des e-mails (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Net.Mail;
 
 namespace media_07
 {
@@ -18,7 +21,40 @@ namespace media_07
       var conf = "USER,PASS,HOST,PORT,SSL".Split(',').ToDictionary(
           p => p, p => Environment.GetEnvironmentVariable($"SMTP_{p}")
       );
+      using SmtpClient smtp = new(conf["HOST"], int.Parse(conf["PORT"] ?? "25"))
+      {
+        Credentials = new NetworkCredential(conf["USER"], conf["PASS"]),
+        EnableSsl = conf["SSL"] == "true"
+      };
 
+      try
+      {
+        MailMessage message = new()
+        {
+          IsBodyHtml = true,
+          From = new MailAddress(conf["USER"] ?? "anonyme@example.com", "Inconnu"),
+          Subject = "Test",
+          Body = """
+            <html>
+                <p>Bonjour,</p>
+                <p>Message bien envoyé avec :
+                <ul><li>Du HTML<li>Une PJ</ul>
+                <p>Merci,</p>
+                <p>Sylvain</p>
+            </html>
+            """
+        };
+        Attachment pj = new Attachment("obj/Debug/net7.0/media_07.pdb");
+
+        message.Attachments.Add(pj);
+        message.To.Add(new MailAddress("contact@syllab.com"));
+        smtp.Send(message);
+        Console.WriteLine("Message envoyé");
+      }
+      catch (Exception e)
+      {
+        Console.Error.WriteLine(e.Message);
+      }
     }
   }
 }
