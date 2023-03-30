05a02ab10031f18664198c76fb8b3778ef7ca7b0
Labasse <labasse@gmail.com>
2023-03-30 15:42:42 +0200

    Tracer des formes (début)

---
+++ src/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Versioning;
+
+namespace media_04
+{
+  class Program
+  {
+    [SupportedOSPlatform("windows")]
+    static void Main(string[] args)
+    {
+      using var img = Image.FromFile("chat.jpeg");
+
+      img.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+      using var vignette = img.GetThumbnailImage(
+          img.Width / 10,
+          img.Height / 10,
+          () => false,
+          IntPtr.Zero
+      );
+      vignette.Save("vignette-chat.png", ImageFormat.Png);
+    }
+  }
+}
