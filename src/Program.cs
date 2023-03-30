5e08dc19bb921c8db9d9f00bab038c2b2e4246a3
Labasse <labasse@gmail.com>
2023-03-30 16:04:39 +0200

    Imprimer ou générer un PDF (début)

---
+++ src/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Versioning;
+
+namespace media_06
+{
+  [SupportedOSPlatform("windows")]
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        var contenu = Directory.GetFiles("c:/windows");
+
+        foreach (var fichier in contenu)
+        {
+          var infos = new FileInfo(fichier);
+
+          Console.WriteLine($"{infos.Name,-100}{infos.Length,10}");
+        }
+      }
+      catch (Exception e)
+      {
+        Console.Error.WriteLine(e.Message);
+      }
+    }
+  }
+}
