c8f282ba95189d7aff3df7acb6c94f173d4eb1f3
Labasse <labasse@gmail.com>
2023-03-30 15:06:38 +0200

    Diffuser du son (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,17 @@
 ﻿namespace media_02;
+
+using System.Media;
+using System.Runtime.Versioning;
+
+[SupportedOSPlatform("windows")]
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+  static void Main(string[] args)
+  {
+    using SoundPlayer player = new("son.wav");
+
+    player.PlaySync();
+    SystemSounds.Asterisk.Play();
+    Console.WriteLine("Hello World!");
+  }
 }
